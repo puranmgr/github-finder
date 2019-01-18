bb9ee9d792a92ceeebfe6c8518e2d5e9f33a7ab6
puranmgr <puranmgr@gmail.com>
2019-01-18 20:51:14 +0000

    added input event listener

---
+++ app.js
@@ -0,0 +1,13 @@
+// Search input 
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener 
+searchUser / addEventListener('keyup', (e) => {
+    // Get input text 
+    const userText = e.target.value;
+
+    if (userText !== '') {
+
+    }
+
+});
