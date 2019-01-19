47d713410159a6ae8d49cf785444a8842dc1deb8
puranmgr <puranmgr@gmail.com>
2019-01-19 18:21:21 +0000

    added repo feature

--- app.js
+++ app.js
@@ -22,9 +22,8 @@ searchUser.addEventListener('keyup', (e) => {
                 } else {
                     // Show profile 
                     ui.showProfile(data.profile);
-
+                    ui.showRepos(data.repos);
                 }
-
             })
     } else {
         // Clear profile
